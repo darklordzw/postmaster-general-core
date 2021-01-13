aef3387bbd6217f99e859572dd0e0b1c8d8f27d1
Ben Whatley <bwhatley@kaneva.com>
2021-01-13 08:13:10 -0500

    Update npm packages, modernize docs, clean up some dead code

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-'use strict';
-
 /**
  * Public interface for the package.
  * @module index
  */
 
-const errors = require('./lib/errors');
-const Transport = require('./lib/transport');
+const errors = require("./lib/errors");
+const Transport = require("./lib/transport");
 
 module.exports = {
-	/** Errors module. */
-	errors,
+  /** Errors module. */
+  errors,
 
-	/** Transport class. */
-	Transport
+  /** Transport class. */
+  Transport,
 };
--- test/transport.test.js
+++ test/transport.test.js
@@ -1,13 +1,12 @@
 ﻿/* eslint import/no-unassigned-import: 'off' */
 /* eslint no-unused-vars: 'off' */
-'use strict';
 
-const chai = require('chai');
-const dirtyChai = require('dirty-chai');
-const Promise = require('bluebird');
-const sinon = require('sinon');
-const Transport = require('../lib/transport');
-const defaults = require('../defaults.json');
+const chai = require("chai");
+const dirtyChai = require("dirty-chai");
+const Promise = require("bluebird");
+const sinon = require("sinon");
+const Transport = require("../lib/transport");
+const defaults = require("../defaults.json");
 
 /* This sets up the Chai assertion library. "should" and "expect"
 initialize their respective assertion properties. The "use()" functions
@@ -18,309 +17,319 @@ const { expect } = chai;
 chai.should();
 chai.use(dirtyChai);
 
-describe('transport:', () => {
-	let sandbox;
+describe("transport:", () => {
+  let sandbox;
 
-	before(() => {
-		sandbox = sinon.createSandbox();
-	});
+  before(() => {
+    sandbox = sinon.createSandbox();
+  });
 
-	afterEach(() => {
-		sandbox.reset();
-	});
+  afterEach(() => {
+    sandbox.reset();
+  });
 
-	describe('constructor:', () => {
-		it('should properly initialize settings from defaults', () => {
-			const transport = new Transport();
-			transport.timingsResetInterval.should.equal(defaults.timingsResetInterval);
-		});
-		it('should properly initialize settings from input', () => {
-			const transport = new Transport({ timingsResetInterval: 100 });
-			transport.timingsResetInterval.should.equal(100);
-		});
-		it('should error on invalid input', () => {
-			try {
-				const transport = new Transport({ timingsResetInterval: 'bob' });
-			} catch (err) {
-				return;
-			}
-			throw new Error('Failed to catch invalid input.');
-		});
-	});
+  describe("constructor:", () => {
+    it("should properly initialize settings from defaults", () => {
+      const transport = new Transport();
+      transport.timingsResetInterval.should.equal(defaults.timingsResetInterval);
+    });
+    it("should properly initialize settings from input", () => {
+      const transport = new Transport({ timingsResetInterval: 100 });
+      transport.timingsResetInterval.should.equal(100);
+    });
+    it("should error on invalid input", () => {
+      try {
+        const transport = new Transport({ timingsResetInterval: "bob" });
+      } catch (err) {
+        return;
+      }
+      throw new Error("Failed to catch invalid input.");
+    });
+  });
 
-	describe('connect:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.connect();
-		});
-	});
+  describe("connect:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.connect();
+    });
+  });
 
-	describe('disconnect:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.disconnect();
-		});
-		it('should cleanup resources', () => {
-			const transport = new Transport();
-			transport.listening = true;
-			return transport.disconnect()
-				.then(() => {
-					transport.listening.should.be.false();
-				});
-		});
-	});
+  describe("disconnect:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.disconnect();
+    });
+    it("should cleanup resources", () => {
+      const transport = new Transport();
+      transport.listening = true;
+      return transport.disconnect().then(() => {
+        transport.listening.should.be.false();
+      });
+    });
+  });
 
-	describe('resolveTopic:', () => {
-		it('should catch invalid input', () => {
-			try {
-				const transport = new Transport();
-				transport.resolveTopic(3353553);
-			} catch (err) {
-				return;
-			}
-			throw new Error('Failed to catch invalid input.');
-		});
-		it('should return the decoded input', () => {
-			const transport = new Transport();
-			const result = transport.resolveTopic('bob');
-			result.should.equal('bob');
-		});
-	});
+  describe("resolveTopic:", () => {
+    it("should catch invalid input", () => {
+      try {
+        const transport = new Transport();
+        transport.resolveTopic(3353553);
+      } catch (err) {
+        return;
+      }
+      throw new Error("Failed to catch invalid input.");
+    });
+    it("should return the decoded input", () => {
+      const transport = new Transport();
+      const result = transport.resolveTopic("bob");
+      result.should.equal("bob");
+    });
+  });
 
-	describe('addMessageListener:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.addMessageListener('bob', () => {
-				return Promise.resolve();
-			});
-		});
-		it('should catch invalid routingKey params', () => {
-			const transport = new Transport();
-			return transport.addMessageListener(44444, () => {
-				return Promise.resolve();
-			})
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should catch invalid callback params', () => {
-			const transport = new Transport();
-			return transport.addMessageListener('bob')
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should register a callback with appropriate params', () => {
-			const transport = new Transport();
-			const spy = sinon.spy(() => {
-				return Promise.resolve();
-			});
-			const spy2 = sinon.spy(transport, 'recordTiming');
-			return transport.addMessageListener('bob', spy)
-				.then((handler) => handler({ test: true }))
-				.then((handler) => {
-					spy.calledOnce.should.be.true();
-					spy.calledWith({ test: true }).should.be.true();
-					spy2.calledOnce.should.be.true();
-					spy.resetHistory();
-					spy2.resetHistory();
-				})
-				.then(() => transport.addMessageListener('bob2', spy))
-				.then((handler) => handler({ test: true, correlationId: 'ggg', initiator: 'fff' }))
-				.then((handler) => {
-					spy.calledOnce.should.be.true();
-					spy2.calledOnce.should.be.true();
-					spy.calledWith({ test: true, correlationId: 'ggg', initiator: 'fff' }).should.be.true();
-				});
-		});
-	});
+  describe("addMessageListener:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.addMessageListener("bob", () => {
+        return Promise.resolve();
+      });
+    });
+    it("should catch invalid routingKey params", () => {
+      const transport = new Transport();
+      return transport
+        .addMessageListener(44444, () => {
+          return Promise.resolve();
+        })
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should catch invalid callback params", () => {
+      const transport = new Transport();
+      return transport
+        .addMessageListener("bob")
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should register a callback with appropriate params", () => {
+      const transport = new Transport();
+      const spy = sinon.spy(() => {
+        return Promise.resolve();
+      });
+      const spy2 = sinon.spy(transport, "recordTiming");
+      return transport
+        .addMessageListener("bob", spy)
+        .then((handler) => handler({ test: true }))
+        .then((handler) => {
+          spy.calledOnce.should.be.true();
+          spy.calledWith({ test: true }).should.be.true();
+          spy2.calledOnce.should.be.true();
+          spy.resetHistory();
+          spy2.resetHistory();
+        })
+        .then(() => transport.addMessageListener("bob2", spy))
+        .then((handler) => handler({ test: true, correlationId: "ggg", initiator: "fff" }))
+        .then((handler) => {
+          spy.calledOnce.should.be.true();
+          spy2.calledOnce.should.be.true();
+          spy.calledWith({ test: true, correlationId: "ggg", initiator: "fff" }).should.be.true();
+        });
+    });
+  });
 
-	describe('recordTiming:', () => {
-		it('should catch invalid topic input', () => {
-			try {
-				const transport = new Transport();
-				transport.recordTiming(3353553, new Date().getTime());
-			} catch (err) {
-				return;
-			}
-			throw new Error('Failed to catch invalid input.');
-		});
-		it('should catch invalid start input', () => {
-			try {
-				const transport = new Transport();
-				transport.recordTiming('bob', 'invalid');
-			} catch (err) {
-				return;
-			}
-			throw new Error('Failed to catch invalid input.');
-		});
-		it('should calculate the timing stats', () => {
-			const transport = new Transport();
-			transport.recordTiming('bob', new Date().getTime());
-			expect(transport.timings.bob).to.exist();
-			transport.timings.bob.messageCount.should.equal(1);
-			transport.timings.bob.minElapsedTime.should.equal(transport.timings.bob.elapsedTime);
-			transport.timings.bob.maxElapsedTime.should.equal(transport.timings.bob.elapsedTime);
-		});
-	});
+  describe("recordTiming:", () => {
+    it("should catch invalid topic input", () => {
+      try {
+        const transport = new Transport();
+        transport.recordTiming(3353553, new Date().getTime());
+      } catch (err) {
+        return;
+      }
+      throw new Error("Failed to catch invalid input.");
+    });
+    it("should catch invalid start input", () => {
+      try {
+        const transport = new Transport();
+        transport.recordTiming("bob", "invalid");
+      } catch (err) {
+        return;
+      }
+      throw new Error("Failed to catch invalid input.");
+    });
+    it("should calculate the timing stats", () => {
+      const transport = new Transport();
+      transport.recordTiming("bob", new Date().getTime());
+      expect(transport.timings.bob).to.exist();
+      transport.timings.bob.messageCount.should.equal(1);
+      transport.timings.bob.minElapsedTime.should.equal(transport.timings.bob.elapsedTime);
+      transport.timings.bob.maxElapsedTime.should.equal(transport.timings.bob.elapsedTime);
+    });
+  });
 
-	describe('resetTimings:', () => {
-		it('should reset the timings', () => {
-			const transport = new Transport();
-			return transport.addMessageListener('bob', () => {
-				return Promise.resolve();
-			})
-				.then(() => transport.recordTiming('bob', new Date().getTime()))
-				.then(() => {
-					expect(transport.timings.bob).to.exist();
-				})
-				.then(() => transport.resetTimings())
-				.then(() => {
-					expect(transport.timings.bob).to.not.exist();
-				});
-		});
-	});
+  describe("resetTimings:", () => {
+    it("should reset the timings", () => {
+      const transport = new Transport();
+      return transport
+        .addMessageListener("bob", () => {
+          return Promise.resolve();
+        })
+        .then(() => transport.recordTiming("bob", new Date().getTime()))
+        .then(() => {
+          expect(transport.timings.bob).to.exist();
+        })
+        .then(() => transport.resetTimings())
+        .then(() => {
+          expect(transport.timings.bob).to.not.exist();
+        });
+    });
+  });
 
-	describe('removeMessageListener:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.removeMessageListener('bob');
-		});
-		it('should catch invalid routingKey params', () => {
-			const transport = new Transport();
-			return transport.removeMessageListener(35353535)
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should remove timing data for the listener', () => {
-			const transport = new Transport();
-			return transport.addMessageListener('bob', () => {
-				return Promise.resolve();
-			})
-				.then(() => transport.recordTiming('bob', new Date().getTime()))
-				.then(() => {
-					expect(transport.timings.bob).to.exist();
-				})
-				.then(() => transport.removeMessageListener('bob'))
-				.then(() => {
-					expect(transport.timings.bob).to.not.exist();
-				});
-		});
-	});
+  describe("removeMessageListener:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.removeMessageListener("bob");
+    });
+    it("should catch invalid routingKey params", () => {
+      const transport = new Transport();
+      return transport
+        .removeMessageListener(35353535)
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should remove timing data for the listener", () => {
+      const transport = new Transport();
+      return transport
+        .addMessageListener("bob", () => {
+          return Promise.resolve();
+        })
+        .then(() => transport.recordTiming("bob", new Date().getTime()))
+        .then(() => {
+          expect(transport.timings.bob).to.exist();
+        })
+        .then(() => transport.removeMessageListener("bob"))
+        .then(() => {
+          expect(transport.timings.bob).to.not.exist();
+        });
+    });
+  });
 
-	describe('listen:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.listen();
-		});
-		it('should start listening', () => {
-			const transport = new Transport();
-			return transport.listen()
-				.then(() => {
-					transport.listening.should.be.true();
-				});
-		});
-	});
+  describe("listen:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.listen();
+    });
+    it("should start listening", () => {
+      const transport = new Transport();
+      return transport.listen().then(() => {
+        transport.listening.should.be.true();
+      });
+    });
+  });
 
-	describe('publish:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.publish('bob');
-		});
-		it('should catch invalid routingKey params', () => {
-			const transport = new Transport();
-			return transport.publish(35353535)
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should catch invalid correlationId params', () => {
-			const transport = new Transport();
-			return transport.publish('bob', {}, { correlationId: 44444 })
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should catch invalid initiator params', () => {
-			const transport = new Transport();
-			return transport.publish('bob', {}, { initiator: 44444 })
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-	});
+  describe("publish:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.publish("bob");
+    });
+    it("should catch invalid routingKey params", () => {
+      const transport = new Transport();
+      return transport
+        .publish(35353535)
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should catch invalid correlationId params", () => {
+      const transport = new Transport();
+      return transport
+        .publish("bob", {}, { correlationId: 44444 })
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should catch invalid initiator params", () => {
+      const transport = new Transport();
+      return transport
+        .publish("bob", {}, { initiator: 44444 })
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+  });
 
-	describe('request:', () => {
-		it('should return a promise that resolves', () => {
-			const transport = new Transport();
-			return transport.request('bob');
-		});
-		it('should catch invalid routingKey params', () => {
-			const transport = new Transport();
-			return transport.request(35353535)
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should catch invalid correlationId params', () => {
-			const transport = new Transport();
-			return transport.request('bob', {}, { correlationId: 44444 })
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-		it('should catch invalid initiator params', () => {
-			const transport = new Transport();
-			return transport.request('bob', {}, { initiator: 44444 })
-				.then(() => {
-					throw new Error('Failed to catch invalid input.');
-				})
-				.catch((err) => {
-					if (!(err instanceof TypeError)) {
-						throw err;
-					}
-				});
-		});
-	});
+  describe("request:", () => {
+    it("should return a promise that resolves", () => {
+      const transport = new Transport();
+      return transport.request("bob");
+    });
+    it("should catch invalid routingKey params", () => {
+      const transport = new Transport();
+      return transport
+        .request(35353535)
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should catch invalid correlationId params", () => {
+      const transport = new Transport();
+      return transport
+        .request("bob", {}, { correlationId: 44444 })
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+    it("should catch invalid initiator params", () => {
+      const transport = new Transport();
+      return transport
+        .request("bob", {}, { initiator: 44444 })
+        .then(() => {
+          throw new Error("Failed to catch invalid input.");
+        })
+        .catch((err) => {
+          if (!(err instanceof TypeError)) {
+            throw err;
+          }
+        });
+    });
+  });
 });
